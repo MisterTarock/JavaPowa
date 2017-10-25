bb00f16f41a8b51a6b581f8a652c87200255df8f
MisterTarock <victor_pb@hotmail.fr>
2017-10-25 10:18:11 +0200

    first class

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    private int dob=1992; //DateOfBirth
+    private int actualyear=2017;
+    private int age=25;
+    private String name="Victor";
+
+    public static void main(String[] args){
+        System.out.print("Hello World !");
+    }
+}
+
